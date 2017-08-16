61039e9536a7904866d6285ba8e546d19ce8756d
renzon <renzon@gmail.com>
2017-08-15 21:42:43 -0300

    Utilização de url for nos handlers

--- backend/conftest.py
+++ backend/conftest.py
@@ -7,9 +7,17 @@ import pytest
 import config_blueprints
 from core.main import app
 
+app.config.update(SERVER_NAME='localhost')
+
 config_blueprints.main()
 
 
 @pytest.fixture
 def test_client():
     return app.test_client()
+
+
+@pytest.fixture(autouse=True)
+def app_context():
+    with app.app_context() as context:
+        yield context
--- backend/core/tests/test_main.py
+++ backend/core/tests/test_main.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+import pytest
+from flask import url_for
 
 
+@pytest.fixture
+def home_resp(test_client):
+    resp = test_client.get(url_for('hello'))
+    return resp
 
-def test_status_code(test_client):
-    resp = test_client.get('/')
-    assert 200 == resp.status_code
+
+def test_status_code(home_resp):
+    assert 200 == home_resp.status_code
+
+
+def test_subscription_link(home_resp):
+    assert url_for('subscriptions.form',
+                   _external=False) in home_resp.data.decode('utf8')
